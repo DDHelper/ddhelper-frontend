93b7dd7a4d98e5005d78e48e1a8c08f861acb9ec
phynon <tallusgreenland@outlook.com>
2021-11-21 15:11:42 +0800

    feat(auth-page): :art: reconstruct page view

--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -2,84 +2,123 @@ import TextField from '@mui/material/TextField';
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useApi } from '../utils/apiClient';
-import { Button, Paper, Box, Stack } from '@mui/material';
+import {
+  Button,
+  Paper,
+  Box,
+  Stack,
+  Grid,
+  CssBaseline,
+  Typography,
+  Link,
+  Avatar,
+  Checkbox,
+  FormControlLabel,
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Controller, useForm } from 'react-hook-form';
-import { InterfaceType } from 'typescript';
 
+const theme = createTheme();
 
-const FormWithHookForm: React.FC<{}> = () => {
+const LoginFormWithHook: React.FC<{}> = () => {
   const { handleSubmit, reset, control } = useForm();
   const { postLogin } = useApi();
   const onSubmit = (data: any) => {
     console.log(data);
     let value = Object.assign(data, {});
-    const response = postLogin(data);
-    console.log(response)
+    const response = postLogin(value);
+    console.log(response);
   };
 
   return (
-    <Paper>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Box
-          component='form'
-          noValidate
-          sx={{ mt: 1 }}
-        >
-          <form>
-            <Stack spacing={2}>
-              <Controller
-                name={'username'}
-                control={control}
-                render={({ field: { onChange, value } }) => (
-                  <TextField
-                    margin='normal'
-                    required
-                    fullWidth
-                    onChange={onChange}
-                    value={value}
-                    label={'User ID'} />
-                )}
-              />
-              <Controller
-                name={'password'}
-                control={control}
-                render={({ field: { onChange, value } }) => (
-                  <TextField
-                    margin='normal'
-                    required
-                    fullWidth
-                    onChange={onChange}
-                    value={value}
-                    label={'Password'} />
-                )}
-              />
-              <Button
-                onClick={handleSubmit(onSubmit)}
-              >
-                Submit
-              </Button>
-              <Button onClick={() => reset()} variant={'outlined'}>Reset</Button>
-            </Stack>
-          </form>
-        </Box>
-      </Box>
-    </Paper>
+    <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 1 }}>
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        label="Email Address"
+        name="email"
+        autoComplete="email"
+        autoFocus
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+      />
+      <FormControlLabel
+        control={<Checkbox value="remember" color="primary" />}
+        label="Remember me"
+      />
+      <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+        Sign In
+      </Button>
+      <Grid container>
+        <Grid item xs>
+          <Link href="#" variant="body2">
+            Forgot password?
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link href="#" variant="body2">
+            {"Don't have an account? Sign Up"}
+          </Link>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
-
 const AuthView = () => {
   return (
-    <div>
-      <FormWithHookForm />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(https://source.unsplash.com/random)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              DD Helper
+            </Typography>
+            <Typography component="h1" variant="h6">
+              Sign in
+            </Typography>
+            <LoginFormWithHook />
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 };
 
