26d0f7b7014c9443a74e5c62c44293e00c473402
szy17 <1164263286@qq.com>
2021-12-07 23:38:15 +0800

    feat: follow/unfolow/group setting buttons

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -25,16 +25,79 @@ import AssignmentIndOutlinedIcon from '@mui/icons-material/AssignmentIndOutlined
 import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
-
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
+import ArrowDropDownCircleIcon from '@mui/icons-material/ArrowDropDownCircle';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Controller, useForm } from 'react-hook-form';
 import PageHeader from './parts/header';
 import PageSider from './parts/sider';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+
+import { blue } from '@mui/material/colors';
 
 const theme = createTheme();
 const drawerWidth = 240;
+const group = ['group setting'];
+export interface SimpleDialogProps {
+  open: boolean;
+  selectedValue: string;
+  onClose: (value: string) => void;
+}
+
+
+function SimpleDialog(props: SimpleDialogProps) {
+  const { onClose, selectedValue, open } = props;
+
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
+  
+  const handleListItemClick = (value: string) => {
+    onClose(value);
+  };
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      {/* <DialogTitle>Set backup account</DialogTitle> */}
+      <List sx={{ pt: 0 }}>
+        {group.map((group) => (
+          <ListItem button onClick={() => handleListItemClick(group)} key={group}>
+            <ListItemAvatar>
+              <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
+             
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={group} />
+          </ListItem>
+        ))}
+        <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
+          <ListItemAvatar>
+            <Avatar>
+              <RemoveIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Unfollow" />
+        </ListItem>
+      </List>
+    </Dialog>
+  );
+}
 
   const SearchView: React.FC<{}> = () => {
+    const [open, setOpen] = React.useState(false);
+    const [selectedValue, setSelectedValue] = React.useState(group[1]);
+  
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = (value: string) => {
+      setOpen(false);
+      setSelectedValue(value);
+    };
+
+
     return (
       <ThemeProvider theme={theme}>
         <Box sx={{ display: 'flex' }}>
@@ -49,29 +112,80 @@ const drawerWidth = 240;
             <Toolbar />
             <Typography paragraph>
               Search
-              <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+              <List sx={{ width: '100%', maxWidth: 1080,  boxShadow: 2,bgcolor: 'background.paper' }}>
+
               <ListItem>
         <ListItemAvatar>
           <Avatar>
           <Avatar alt="1" src="/imgs/1.jpg" />
           </Avatar>
         </ListItemAvatar>
-        <ListItemText primary="Account 1" secondary="blablabla" />
+        <Button variant="outlined" startIcon={<AddIcon />} sx={{
+           p: 2,
+          position: 'absolute',
+          left: '80%',
+          zIndex: 'tooltip',
+        }}
+>
+        Follow
+      </Button>
+        {/* <ListItemText primary="Account 1" secondary="blablablablablablablablablablablablablablabla" /> */}
+        <ListItemText
+          primary="Summer BBQ"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                to Scott, Alex, Jennifer
+              </Typography>
+              {" — Wish I could come, but I'm out of town this…"}
+            </React.Fragment>
+          }
+        />
       </ListItem>
+      <Divider />
       <ListItem>
         <ListItemAvatar>
           <Avatar>
           <Avatar alt="2" src="/imgs/2.jpg" />
           </Avatar>
         </ListItemAvatar>
+        <Button variant="outlined" startIcon={<AddIcon />} sx={{
+           p: 2,
+          position: 'absolute',
+          left: '80%',
+          zIndex: 'tooltip',
+        }}
+>
+        Follow
+      </Button>
         <ListItemText primary="Account 2" secondary="blablabla" />
       </ListItem>
+      <Divider />
       <ListItem>
         <ListItemAvatar>
           <Avatar>
           <Avatar alt="3" src="/imgs/3.jpg" />
           </Avatar>
         </ListItemAvatar>
+        <Button variant="contained" startIcon={<ArrowDropDownCircleIcon />} sx={{
+           p: 2,
+          position: 'absolute',
+          left: '80%',
+          zIndex: 'tooltip',
+        }} onClick={handleClickOpen}
+>
+        Followed
+      </Button>
+      <SimpleDialog
+        selectedValue={selectedValue}
+        open={open}
+        onClose={handleClose}
+      />
         <ListItemText primary="Account 3" secondary="blablabla" />
       </ListItem>
 
