6e255caa24f5536b72683deb81423591a06dd62e
Phynon <tallusgreenland@outlook.com>
2021-12-12 10:37:39 +0800

    refactor(api model): add search grouplist and subscribe

--- src/utils/apiModels.d.ts
+++ src/utils/apiModels.d.ts
@@ -1,8 +1,16 @@
+// post Login
 export interface LoginValues {
   username: string;
   password: string;
 }
 
+export interface LoginApiReturn {
+  code: number;
+  msg?: string;
+  data?: object;
+}
+
+// post Register
 export interface RegisterValues {
   username: string;
   password: string;
@@ -15,12 +23,6 @@ export interface PinValues {
   email: string;
 }
 
-export interface LoginApiReturn {
-  code: number;
-  msg?: string;
-  data?: object;
-}
-
 export interface RegisterApiReturn {
   code: number;
   msg: string;
@@ -31,6 +33,7 @@ export interface PinApiReturn {
   msg: string;
 }
 
+// get User
 export interface UserModel {
   username: string;
   uid: number;
@@ -46,6 +49,7 @@ export interface UserApiReturn {
   };
 }
 
+// get SearchSubscribe
 export interface SearchSubsribeApiReturn {
   code: number;
   data: {
@@ -56,6 +60,27 @@ export interface SearchSubsribeApiReturn {
     upic: string;
     raw: object;
   };
+}
+
+// post DoSubscribe
+export interface DoSubscribeValues {
+  mid: number;
+  gid: Array<number>;
+}
+
+export interface DoSubscribeApiReturn {
+  code: number;
+  msg: string;
+}
+
+// get GroupList
+export interface GroupListApiReturn {
+  code: number;
+  data: Array<{
+    gid: number;
+    group_name: string;
+    count: number;
+  }>;
 }
 
 export interface UserMetadatumModel {
