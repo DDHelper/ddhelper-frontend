3669c727c28abb4b79636142d3e494644f8d355a
phynon <tallusgreenland@outlook.com>
2021-12-17 16:09:28 +0800

    feat(dynamic): :construction: compl video card

--- src/components/mainrolllist.tsx
+++ src/components/mainrolllist.tsx
@@ -40,6 +40,8 @@ import { useApi } from '../utils/apiClient';
 import { DynamicApiReturn, GroupListApiReturn } from '../utils/apiModels';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import Link from '@mui/material/Link';
+import { Chip } from '@mui/material';
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
@@ -58,6 +60,17 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
 
 const theme = createTheme();
 const drawerWidth = 240;
+
+function timestampToTime(timestamp: number) {
+  let date = new Date(timestamp * 1000); //timestamp 为10位需*1000，timestamp 为13位的话不需乘1000
+  let Y = date.getFullYear() + '-';
+  let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+  let D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' ';
+  let h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+  let m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+  let s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+  return Y + M + D + h + m + s;
+}
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -130,17 +143,6 @@ const RolllistItem: React.FC<{ gid: number }> = (props) => {
   const [pageOffset, setPageOffset] = useState<number>(0);
   const [dynamicData, setDynamicData] = useState<DynamicApiReturn>();
 
-  function timestampToTime(timestamp: number) {
-    let date = new Date(timestamp * 1000); //timestamp 为10位需*1000，timestamp 为13位的话不需乘1000
-    let Y = date.getFullYear() + '-';
-    let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
-    let D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' ';
-    let h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
-    let m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
-    let s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
-    return Y + M + D + h + m + s;
-  }
-
   useEffect(() => {
     async function fetch() {
       const dynamicResponse = await getDynamic({
@@ -156,43 +158,6 @@ const RolllistItem: React.FC<{ gid: number }> = (props) => {
     fetch();
   }, [getDynamic, props.gid]);
 
-  /*
-  return (
-    <List sx={{ width: '100%', maxWidth: 1000, bgcolor: 'background.paper' }}>
-      <ListItem alignItems="flex-start">
-        <Card sx={{ maxWidth: 1000 }}>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                R
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="settings">
-                <MoreVertIcon />
-              </IconButton>
-            }
-            title="up主名称"
-            subheader="动态发布时间"
-          />
-          <CardMedia
-            component="img"
-            height="194"
-            image="/static/images/cards/paella.jpg"
-            alt="动态图片"
-          />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              动态内容测试长度测试长度测试长度 测试长度 测试长度 测试长度 测试长度 测试长度 测试长度
-              测试长度 测试长度 测试长度 测试长度 测试长度
-            </Typography>
-          </CardContent>
-        </Card>
-      </ListItem>
-
-    </List>
-  );
-  */
   return loaded ? (
     <List
       sx={{
@@ -202,6 +167,7 @@ const RolllistItem: React.FC<{ gid: number }> = (props) => {
     >
       {dynamicData!.data.data.map((item, idx) => {
         const card = JSON.parse(item.raw.card);
+        const desc = item.raw.desc;
         const dtype = item.dynamic_type;
         console.log(dtype);
         let uname = '';
@@ -209,10 +175,12 @@ const RolllistItem: React.FC<{ gid: number }> = (props) => {
         let face = '';
         switch (dtype) {
           case 1:
-            uname = card.user.uname;
-            uid = card.user.uid;
-            face = card.user.face;
-            break;
+            return (
+              <ListItem alignItems="flex-start">
+                <ItemType1 card={card} desc={desc} time={timestampToTime(item.timestamp)} />
+              </ListItem>
+            );
+
           case 2:
             uname = card.user.name;
             uid = card.user.uid;
@@ -224,10 +192,11 @@ const RolllistItem: React.FC<{ gid: number }> = (props) => {
             face = card.user.face;
             break;
           case 8:
-            uname = card.owner.name;
-            uid = card.owner.mid;
-            face = card.owner.face;
-            break;
+            return (
+              <ListItem alignItems="flex-start">
+                <ItemType8 card={card} desc={desc} time={timestampToTime(item.timestamp)} />
+              </ListItem>
+            );
         }
 
         return (
@@ -261,6 +230,192 @@ const RolllistItem: React.FC<{ gid: number }> = (props) => {
     </Typography>
   );
 };
+
+const ItemType1: React.FC<{ card: any; desc: any; time: string }> = (props) => {
+  // repost
+  let uname = props.card.user.uname;
+  let uid = props.card.user.uid;
+  let face = props.card.user.face;
+  let time = props.time;
+  let item = props.card.item;
+  let origin = props.card.origin;
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar
+            variant="circular"
+            src={`http://ddd.edrows.top/txcos/pic/?url=${face}@60w_60h.webp`}
+          />
+        }
+        title={uname}
+        subheader={time}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {item.content}
+        </Typography>
+        <Divider />
+        <Card>
+          <CardHeader
+            avatar={
+              <Avatar
+                variant="circular"
+                src={`http://ddd.edrows.top/txcos/pic/?url=${face}@60w_60h.webp`}
+              />
+            }
+            title={uname}
+            subheader={timestampToTime(item.timestamp)}
+          />
+          <CardMedia component="img" height="194" image="/static/images/cards/paella.jpg" />
+          <CardContent></CardContent>
+        </Card>
+      </CardContent>
+    </Card>
+  );
+};
+
+const ItemType2: React.FC<{ card: any; desc: any; time: string }> = (props) => {
+  // repost
+  let uname = props.card.user.uname;
+  let uid = props.card.user.uid;
+  let face = props.card.user.face;
+  let time = props.time;
+  let item = props.card.item;
+  let origin = props.card.origin;
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar
+            variant="circular"
+            src={`http://ddd.edrows.top/txcos/pic/?url=${face}@60w_60h.webp`}
+          />
+        }
+        title={uname}
+        subheader={time}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {item.content}
+        </Typography>
+        <Divider />
+        <Card>
+          <CardHeader
+            avatar={
+              <Avatar
+                variant="circular"
+                src={`http://ddd.edrows.top/txcos/pic/?url=${face}@60w_60h.webp`}
+              />
+            }
+            title={uname}
+            subheader={timestampToTime(item.timestamp)}
+          />
+          <CardMedia component="img" height="194" image="/static/images/cards/paella.jpg" />
+          <CardContent></CardContent>
+        </Card>
+      </CardContent>
+    </Card>
+  );
+};
+
+const ItemType4: React.FC<{ card: any; desc: any; time: string }> = (props) => {
+  // repost
+  let uname = props.card.user.uname;
+  let uid = props.card.user.uid;
+  let face = props.card.user.face;
+  let time = props.time;
+  let item = props.card.item;
+  let origin = props.card.origin;
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar
+            variant="circular"
+            src={`http://ddd.edrows.top/txcos/pic/?url=${face}@60w_60h.webp`}
+          />
+        }
+        title={uname}
+        subheader={time}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {item.content}
+        </Typography>
+        <Divider />
+        <Card>
+          <CardHeader
+            avatar={
+              <Avatar
+                variant="circular"
+                src={`http://ddd.edrows.top/txcos/pic/?url=${face}@60w_60h.webp`}
+              />
+            }
+            title={uname}
+            subheader={timestampToTime(item.timestamp)}
+          />
+          <CardMedia component="img" height="194" image="/static/images/cards/paella.jpg" />
+          <CardContent></CardContent>
+        </Card>
+      </CardContent>
+    </Card>
+  );
+};
+
+const ItemType8: React.FC<{ card: any; desc: any; time: string }> = (props) => {
+  // video
+  let uname = props.card.owner.name;
+  let uid = props.card.owner.mid;
+  let face = props.card.owner.face;
+  let time = props.time;
+  let title = props.card.title;
+  let origin = props.card.origin;
+  return (
+    <Card sx={{ display: 'flex', flexDirection: 'column' }}>
+      <CardHeader
+        avatar={
+          <Avatar
+            variant="circular"
+            src={`http://ddd.edrows.top/txcos/pic/?url=${face}@60w_60h.webp`}
+          />
+        }
+        title={uname}
+        subheader={time}
+        action={<Chip label="视频" variant="outlined" />}
+      />
+
+      <CardContent sx={{ display: 'flex', flexDirection: 'row' }}>
+        <Link
+          variant="h6"
+          href={props.card.short_link_v2}
+          color="inherit"
+          underline="hover"
+          sx={{ ml: 2 }}
+        >
+          <CardMedia
+            sx={{ display: 'flex', flexDirection: 'row' }}
+            component="img"
+            image={`http://ddd.edrows.top/txcos/pic/?url=${props.card.pic}@360w.webp`}
+          />
+        </Link>
+        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+          <Link
+            variant="h6"
+            href={props.card.short_link_v2}
+            color="inherit"
+            underline="hover"
+            sx={{ ml: 2, maxWidth: 360}}
+          >
+            {title}
+          </Link>
+          <Typography sx={{ ml: 2 }}>miaoshu</Typography>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+};
+
 const MainrolllistPageView: React.FC<{}> = () => {
   const [expanded, setExpanded] = React.useState(false);
 
